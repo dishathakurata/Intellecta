12c2cae92a56d53fba3a517169ed3f98b3497926
Disha Thakurata <146114938+dishathakurata@users.noreply.github.com>
2024-04-06 21:28:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+from openai import OpenAI
+import streamlit as st
+from dotenv import load_dotenv
+import os
+import shelve
+
+load_dotenv()
+
+st.title("Intellecta")
+
+USER_AVATAR = "👤"
+BOT_AVATAR = "🤖"
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# Ensure openai_model is initialized in session state
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+
+# Load chat history from shelve file
+def load_chat_history():
+    with shelve.open("chat_history") as db:
+        return db.get("messages", [])
+
+
+# Save chat history to shelve file
+def save_chat_history(messages):
+    with shelve.open("chat_history") as db:
+        db["messages"] = messages
+
+
+# Initialize or load chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = load_chat_history()
+
+# Sidebar with a button to delete chat history
+with st.sidebar:
+    if st.button("Delete Chat History"):
+        st.session_state.messages = []
+        save_chat_history([])
+
+# get colors from theme config file, or set the colours to altair standards
+primary_clr = st.get_option("theme.primaryColor")
+background_clr = st.get_option("theme.backgroundColor")
+second_clr = st.get_option("theme.secondaryBackgroundColor")
+txt_clr = st.get_option("theme.textColor")
+
+# Display chat messages
+for message in st.session_state.messages:
+    avatar = USER_AVATAR if message["role"] == "user" else BOT_AVATAR
+    with st.chat_message(message["role"], avatar=avatar):
+        st.markdown(message["content"])
+
+# Main chat interface
+if prompt := st.chat_input("How can I help?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user", avatar=USER_AVATAR):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant", avatar=BOT_AVATAR):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=st.session_state["messages"],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.content or ""
+            message_placeholder.markdown(full_response + "|")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+# Save chat history after each interaction
+save_chat_history(st.session_state.messages)
